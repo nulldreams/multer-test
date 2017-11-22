b604e02213bfa7253e3542447fab1e43670de1d2
igor souza martins <nulldreams@gmail.com>
2017-11-21 22:44:47 -0800

    feat: teste

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + '.png')
+    }
+})
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const upload = multer({
+    storage: storage
+
+})
+
+app.listen(3000, () => {
+    console.log('god is top')
+})
+
+app.post('/animal', upload.single('animal'), teste)
+
+function teste(req, res) {
+    console.log(req.file)
+}
